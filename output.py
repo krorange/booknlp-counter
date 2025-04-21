31f0c156afffe2fd0d3f5f9f76ba19a8a8f68d22
Gyuri Kang <78900024+krorange@users.noreply.github.com>
2025-04-21 10:35:45 -0400

    change variable names

--- output.py
+++ output.py
@@ -1,4 +1,4 @@
-# This is a program that outputs the number of animal/plant/human agents (nsubj)
+# This is a program that outputs the number of animal/plant/human subjects (nsubj)
 
 #paragraph_ID	sentence_ID	token_ID_within_sentence	token_ID_within_document	word	lemma	byte_onset	byte_offset	POS_tag	fine_POS_tag	dependency_relation	syntactic_head_ID	event	supersense_category
 #1	0	0	0	*	*	3	4	PUNCT	NFP	meta	4	O	_
@@ -7,8 +7,8 @@ first = True
 current_sentence_id = 0
 current_sentence = []
 
-# Set file path
-fd = '/FILEPATH/FILENAME.complete'
+# Set file path for the concatenated file
+fd = 'joyce2003.complete'
 line = fd.readline()
 
 # Default number of supersense tags
@@ -30,16 +30,17 @@ while line:
 	sentence_id = int(row[1])
 	if sentence_id != current_sentence_id:
 		for sentence_row in current_sentence:
-			#print(sentence_row)
-			if sentence_row[13] in ['noun.animal'] and sentence_row[10] in ['nsubj']:
+			lemma = sentence_row[13].lower()
+            		dep = sentence_row[10]
+			if lemma in ['noun.animal'] and dep in ['nsubj']:
 				#print(current_sentence_id,':::',' '.join([i[4] for i in current_sentence]))
 				num_ani += 1
 				sum_ani = str(num_ani)
-			if sentence_row[13] in ['noun.plant'] and sentence_row[10] in ['nsubj']:
+			if lemma in ['noun.plant'] and dep in ['nsubj']:
 				#print(current_sentence_id,';;;',' '.join([i[4] for i in current_sentence]))
 				num_pla += 1
 				sum_pla = str(num_ani)
-			if sentence_row[13] in ['noun.person'] and sentence_row[10] in ['nsubj']:
+			if lemma in ['noun.person'] and dep in ['nsubj']:
 				#print(current_sentence_id,'!!!',' '.join([i[4] for i in current_sentence]))
 				num_hum += 1
 				sum_hum = str(num_ani)
